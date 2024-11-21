b86c2925dea5292e14e6c574e8c90545a430a74e
Ehab Barakat <dev.ehab.barakat@gmail.com>
2024-11-21 17:24:47 +0200

    countries seeder

---
+++ Modules/Countries/Database/seeders/cities/kw.php
@@ -0,0 +1,69 @@
+<?php
+
+return [
+    [
+        'name:ar' => 'الكويت',
+        'name:en' => 'Kuwait City',
+        'name:tr' => 'Kuveyt Şehri',
+    ],
+    [
+        'name:ar' => 'السالمية',
+        'name:en' => 'Salmiya',
+        'name:tr' => 'Salmiye',
+    ],
+    [
+        'name:ar' => 'الفروانية',
+        'name:en' => 'Farwaniya',
+        'name:tr' => 'Fervaniye',
+    ],
+    [
+        'name:ar' => 'الجهراء',
+        'name:en' => 'Jahra',
+        'name:tr' => 'Cehra',
+    ],
+    [
+        'name:ar' => 'حولي',
+        'name:en' => 'Hawally',
+        'name:tr' => 'Havalli',
+    ],
+    [
+        'name:ar' => 'مدينة الكويت',
+        'name:en' => 'Kuwait City',
+        'name:tr' => 'Kuveyt Şehri',
+    ],
+    [
+        'name:ar' => 'الرقة',
+        'name:en' => 'Riqqa',
+        'name:tr' => 'Rikka',
+    ],
+    [
+        'name:ar' => 'الصباحية',
+        'name:en' => 'Sabahiya',
+        'name:tr' => 'Sabahiyah',
+    ],
+    [
+        'name:ar' => 'فحيحيل',
+        'name:en' => 'Fahaheel',
+        'name:tr' => 'Fehayhil',
+    ],
+    [
+        'name:ar' => 'المنقف',
+        'name:en' => 'Mangaf',
+        'name:tr' => 'Mengaf',
+    ],
+    [
+        'name:ar' => 'الأحمدي',
+        'name:en' => 'Ahmadi',
+        'name:tr' => 'Ahmedi',
+    ],
+    [
+        'name:ar' => 'مبارك الكبير',
+        'name:en' => 'Mubarak Al-Kabeer',
+        'name:tr' => 'Mubarek el-Kebir',
+    ],
+    [
+        'name:ar' => 'بيان',
+        'name:en' => 'Bayan',
+        'name:tr' => 'Beyan',
+    ],
+];
